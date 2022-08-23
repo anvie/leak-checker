566e8567c7faa4af9ae942c047db364a2d71d9b6
anvie <anvie@users.noreply.github.com>
2022-08-23 07:17:35 +0000

    deploy: 8d78daf91040ad75d543e8be86c6c3bf275c965e

--- _next/static/szXBC7nZVPWiTPymQha3x/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/537-62bdb8902248858d.js","static/css/9ac64045a64db0fc.css","static/chunks/pages/index-a0236f49d44c634f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
