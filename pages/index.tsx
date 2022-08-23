8d78daf91040ad75d543e8be86c6c3bf275c965e
anvie <robinsyihab@gmail.com>
2022-08-23 14:15:48 +0700

    change octocat color and copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -136,8 +136,8 @@ const Home: NextPage = () => {
       <footer className={styles.footer}>
         <div className="flex flex-col items-center pt-10">
           <p className="p-10 text-center md:w-2/3 bg-green-100 md:rounded-xl">
-            Website ini statis bisa bekerja tanpa koneksi internet sehingga aman tidak ada informasi yang dikirim ke server, semua
-            pemeriksaan dilakukan di lokal (browser) tanpa data sumber,
+            Website ini statis bisa bekerja tanpa koneksi internet sehingga aman tidak ada informasi yang keluar, semua
+            pemeriksaan dilakukan di lokal (browser) tanpa data sumber
             menggunakan konsep <a className="link" href="https://en.wikipedia.org/wiki/Zero-knowledge_proof" target="_blank" rel="noreferrer">zero-knowledge proof (ZKP)</a>.
           </p>
           <p className="p-10">
