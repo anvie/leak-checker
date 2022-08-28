fe06b4555ddde04defe7fa3d4be42fa407a46a6e
anvie <robinsyihab@gmail.com>
2022-08-28 12:35:21 +0700

    update _offline.tsx

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -2,17 +2,17 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.sass";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import imageLoader from "../imageLoader";
 
 import { sha256 } from "js-sha256";
 import { Loading } from "../components/Loading";
 import { GithubCorner } from "../components/GithubCorner";
 
-const metranet = require("../lib/metranet.json");
-const unipanca = require("../lib/unipanca.json");
-
-const dbs = [metranet, unipanca];
+// const metranet = require("../lib/metranet.json");
+// const unipanca = require("../lib/unipanca.json");
+const TOTAL_SIGS = 2;
+// const dbs = [];
 
 const isNik = (id: string) => {
   return /^\d{16}$/.test(id);
@@ -32,8 +32,37 @@ const Home: NextPage = () => {
   const [leaked, setLeaked] = useState(0);
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [ready, setReady] = useState(false);
   const [kind, setKind] = useState<string>("");
   const [leakFrom, setLeakFrom] = useState<string[]>([]);
+  const [sigs, setSigs] = useState<any[]>([]);
+  const [sigCount, setSigCount] = useState(0);
+
+
+  useEffect(() => {
+    console.log(
+      "🚀 ~ file: index.tsx ~ line 52 ~ useEffect ~ sigs.length",
+      sigs.length
+    );
+    if (sigs.length >= TOTAL_SIGS) {
+      setReady(true);
+    }
+  }, [sigs, sigCount]);
+
+  useEffect(() => {
+    ["metranet.json", "unipanca.json"].forEach((sig) => {
+      console.log("🚀 ~ file: index.tsx ~ line 42 ~ .forEach ~ sig", sig);
+      fetch(`/sigs/${sig}`).then(async (response) => {
+        let _sigs = sigs;
+        const _db = await response.json();
+        _sigs.push(_db);
+        setSigs(_sigs);
+        console.log("sigs:", sigs);
+        setSigCount(_sigs.length);
+      });
+    });
+  }, []);
+
 
   const checkInput = (e: any) => {
     const query = e.target.value.trim();
@@ -76,8 +105,8 @@ const Home: NextPage = () => {
         setLoading(false);
         let _leaked = 2;
         let leakedFrom = [];
-        for (var i = 0; i < dbs.length; i++) {
-          const db = dbs[i];
+        for (var i = 0; i < sigs.length; i++) {
+          const db = sigs[i];
           if (db.hashes.indexOf(hash) > -1) {
             _leaked = 1;
             leakedFrom.push(db.name);
@@ -98,15 +127,17 @@ const Home: NextPage = () => {
           content="Website to check is your personal data has been leaked."
         />
         <link rel="icon" href="/favicon.ico" />
-        <link rel='manifest' href='/manifest.json' />
       </Head>
       <GithubCorner />
       <main className={`${styles.main} text-center flex flex-col items-center`}>
+
+        <div>offline mode</div>
+
         <h1 className="font-semibold">Apakah data pribadi Anda bocor?</h1>
         <h2>Coba periksa di sini</h2>
-        <h2>You are offline!</h2>
 
-        <div className="w-96 pt-10">
+        {!ready && <div className="p-10"><Loading className="" /> <div>Loading signatures...</div></div>}
+        {ready && <div className="w-96 pt-10">
           <p>Periksa:</p>
           <input
             className="shadow appearance-none border rounded w-full pl-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -116,12 +147,16 @@ const Home: NextPage = () => {
             onKeyUp={checkInput}
             onClick={(e: any) => e.target.select()}
           ></input>
-        </div>
+        </div>}
+
+        
+
+
         <div className="flex flex-col">
           {loading && <Loading className={"pt-10"} />}
           {leaked === 1 && (
             <div className="pt-5 flex flex-col items-center text-center">
-              <div className="text-xl font-bold">LEAK!</div>
+              <div className="text-xl text-red-500 font-bold">LEAK!</div>
               <Image
                 src="leak.jpg"
                 alt="Leak Bali"
