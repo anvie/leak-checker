dd7e527c224a58a8d3b7f4fb8c9a6d0dd158f364
anvie <robinsyihab@gmail.com>
2022-09-02 22:41:13 +0700

    Major optimization updates:
    * Use wasm as the checking engine.
    * Change db signature format from json to lhash
      this significantly optimize speed and reduce data size.
    * Change hashing algorithm to fnv1a 64bit.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,44 @@
+
+#[macro_use] extern crate lazy_static;
+
 use wasm_bindgen::prelude::*;
+use lhash::{LhashParser, hash};
+
+use std::{sync::Mutex, collections::HashMap};
 
 #[wasm_bindgen]
 pub fn add(x: i32, y: i32) -> i32 {
     return x + y;
+}
+
+lazy_static! {
+    static ref LHASH_DATA:Mutex<HashMap<String, Vec<u64>>> = Mutex::new(HashMap::new());
+}
+
+#[wasm_bindgen]
+pub fn load_lhash(name:&str, data: &[u8]) -> u32 {
+    let mut reader = LhashParser::new(data.to_vec());
+    // let mut result = String::new();
+
+    let mut lhash_data = LHASH_DATA.lock().unwrap();
+    let mut sigs = vec![];
+    let mut count = 0;
+    while let Ok(sig) = reader.read() {
+        sigs.push(sig);
+        count += 1;
+    }
+
+    lhash_data.insert(name.to_string(), sigs);
+
+    count
+}
+
+#[wasm_bindgen]
+pub fn hash_exists(name:&str, value: &str) -> bool {
+    let c_hash = hash(value);
+    let lhash_data = LHASH_DATA.lock().unwrap();
+    let sigs = lhash_data.get(name).unwrap();
+    sigs.contains(&c_hash)
 }
 
 #[cfg(test)]
